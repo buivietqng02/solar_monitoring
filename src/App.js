613947e5d73084a56bdc50a765e9106a0231728d
thaihoa1408 <phamthaihoa28@gmail.com>
2023-05-17 16:11:59 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import 'antd/dist/antd.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.scss';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import PublicRoute from './utils/PublicRoute';
+import PrivateRoute from './utils/PrivateRoute';
+import SignIn from './pages/SignIn';
+import DashboardLayout from './layouts/DashboardLayout';
+import SiteView from './pages/SiteView';
+
+function App() {
+  const windowHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty('--window-height', `${window.innerHeight}px`);
+  };
+  window.addEventListener('resize', windowHeight);
+  windowHeight();
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to='/sign-in' />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route element={<DashboardLayout />}>
+          <Route path='/site-monitor/siteview' element={<SiteView />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/SiteView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/SiteView.scss';
+import MainviewLayout from '../layouts/MainviewLayout';
+
+export default function SiteView() {
+  return (
+    <MainviewLayout>
+      <div className='siteview'>SiteView</div>
+    </MainviewLayout>
+  );
+}
